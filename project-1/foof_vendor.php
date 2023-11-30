5705f721e317983bc46b8094dc9109ba11c1ea96
wei <alex20180614@gmail.com>
2023-11-30 10:34:21 -0500

    update PHP

--- project-1/foof_vendor.php
+++ project-1/foof_vendor.php
@@ -53,5 +53,25 @@ class DishIngredient {
     }
 }
 
+$supplier1 = new Supplier(1, 'Farm Fresh Suppliers', 'City A');
+$supplier2 = new Supplier(2, 'Organic Ingredients Inc.', 'City B');
+
+// Create instances of Ingredients
+$ingredient1 = new Ingredient(101, 'Carrots', 'Vegetable', $supplier1->SupplierID, 1.5);
+$ingredient2 = new Ingredient(102, 'Chicken', 'Meat', $supplier2->SupplierID, 5.0);
+
+// Create instances of Dishes
+$dish1 = new Dish(201, 'Vegetable Stir Fry', 'A healthy mix of fresh vegetables');
+$dish2 = new Dish(202, 'Grilled Chicken Salad', 'Grilled chicken with a variety of fresh greens');
+
+// Create instances of DishIngredients
+$dishIngredient1 = new DishIngredient($dish1->DishID, $ingredient1->IngredientID, 2);
+$dishIngredient2 = new DishIngredient($dish2->DishID, $ingredient2->IngredientID, 1);
+
+// Display information
+echo "Supplier: {$supplier1->SupplierName}, Location: {$supplier1->SupplierLocation}\n";
+echo "Ingredient: {$ingredient1->IngredientName}, Type: {$ingredient1->IngredientType}, Price: {$ingredient1->PricePerUnit}\n";
+echo "Dish: {$dish1->DishName}, Description: {$dish1->Description}\n";
+echo "Dish Ingredient: DishID {$dishIngredient1->DishID}, IngredientID {$dishIngredient1->IngredientID}, Quantity: {$dishIngredient1->Quantity}\n";
 
 ?>
